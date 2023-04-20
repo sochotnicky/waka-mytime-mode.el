ca4b98b9c4f5a29a381697edbdf180d57677481b
Stanislav Ochotnický <stanislav@ochotnicky.com>
2023-04-20 22:53:54 +0200

    Define minor mode
    Fair amount of renaming as well

--- waka-mytime-mode.el
+++ waka-mytime-mode.el
@@ -46,56 +46,62 @@
   :group 'wakame-mode)
 
 
-(defcustom wakame-mode-cache-file "~/.cache/wakame-mode.cache"
+(defcustom wakame-cache-file "~/.cache/wakame-mode.cache"
   "File path used for caching WakaTime heartbeats when offline."
   :type 'string
   :group 'wakame-mode)
 
-(defvar wakame-mode-api-key
+(defvar wakame-api-key
   "API Key populated from Auth-Source")
 
-(defvar wakame-mode--heartbeats '()
+(defvar wakame--heartbeats '()
   "List of heartbeats yet to be sent to wakatime.")
 
-(defun wakame-mode--pretty-mode()
+(defun wakame--pretty-mode()
   "Return nice human-readable type of current file/buffer."
   (if (buffer-file-name)
       (if (listp mode-name)
           (car mode-name)
         mode-name)))
 
-(defun wakame-mode--current-type()
+(defvar wakame--last-heartbeat-entity nil
+  "Value of last entity that was added to the heartbeats")
+
+(defvar wakame--last-heartbeat-time nil
+  "Time of of last heartbeat was added")
+
+(defun wakame--current-type()
   "Return whether current buffer is file backed or considered an app."
   (if (buffer-file-name)
       "file"
     "app"))
 
-(defun wakame-mode--current-entity()
+(defun wakame--current-entity()
   "Return path of current entity or app name - i.e. major mode."
   (or (buffer-file-name)
       (symbol-name major-mode)))
 
-(defun wakame-mode--create-heartbeat(savep)
+(defun wakame--create-heartbeat(savep)
   "Return a new heartbeat.
 
 If SAVEP is non-nil record writing heartbeat"
   `(
-    (type . ,(wakame-mode--current-type))
+    (type . ,(wakame--current-type))
     (time . ,(float-time))
     (branch . ,(magit-get-current-branch))
     (project . ,(if (project-current) (project-name (project-current)) ""))
-    (language . ,(wakame-mode--pretty-mode))
+    (language . ,(wakame--pretty-mode))
     (is_write . ,savep)
     ;; See ParseUserAgent at https://github.com/muety/wakapi/blob/master/utils/http.go
     (user_agent . ,(format "wakatime/unset (linux-unset) Emacs emacs-wakatime/1.0"))
-    (entity . ,(wakame-mode--current-entity))))
+    (entity . ,(wakame--current-entity))))
 
-(defun wakame-mode--send-heartbeat(heartbeat)
+(defun wakame--send-heartbeat(heartbeat)
   "Sends heartbeat to configured API server."
   (let ((url-request-method "POST")
         (url-request-data (json-encode heartbeat))
         (url-request-extra-headers
-         `(("Authorization" . ,(format "Basic %s" (base64-encode-string wakame-mode-api-key)))
+         `(("Authorization" . ,(format "Basic %s" (base64-encode-string wakame-api-key)))
            ("Content-Type" . "application/json")
            ("X-Machine-Name" . ,(system-name)))))
     (url-retrieve
@@ -103,35 +109,71 @@ If SAVEP is non-nil record writing heartbeat"
      (lambda(status)
        (if (plist-get status 'error)
            (message "Error posting heartbeat: %s" (plist-get status 'error))
-         (delete heartbeat wakame-mode--heartbeats)))
+         (delete heartbeat wakame--heartbeats)))
      nil
      t
      t)))
 
+(defun wakame--add-heartbeat(heartbeat)
+  "Add heartbeat to heartbeats and track last addition.
+
+Only adds heartbeat if same entity (file/app) has not been added
+to the heartbeat last in past 2 minutes"
+  (let ((entity (alist-get 'entity heartbeat))
+        (timediff (- (float-time) (or wakame--last-heartbeat-time 120))))
+    (if (or (not (equal wakame--last-heartbeat-entity entity))
+            (> timediff 120))
+        (progn
+          (setq wakame--last-heartbeat-time (alist-get 'time heartbeat))
+          (setq wakame--last-heartbeat-entity (alist-get 'entity heartbeat))
+          (add-to-list 'wakame--heartbeats heartbeat)))))
+
 (defun wakame-mode--save-heartbeat(heartbeat)
-  (if (file-exists-p wakame-mode-cache-file)
+  (if (file-exists-p wakame-cache-file)
       (with-temp-buffer
-        (insert-file-contents wakame-mode-cache-file)
+        (insert-file-contents wakame-cache-file)
         (setq-local cache-data (read (current-buffer)))
         (add-to-list 'cache-data heartbeat)
         (delete-region (point-min) (point-max))
         (insert (prin1-to-string cache-data))
-        (write-region (point-min) (point-max) wakame-mode-cache-file nil 'quiet))
+        (write-region (point-min) (point-max) wakame-cache-file nil 'quiet))
     (with-temp-buffer
       (insert (prin1-to-string (cons heartbeat '())))
-      (write-region (point-min) (point-max) wakame-mode-cache-file nil 'quiet))))
+      (write-region (point-min) (point-max) wakame-cache-file nil 'quiet))))
 
-(defun wakame-mode--buffer-change(frame)
-  (add-to-list 'wakame-mode--heartbeats (wakame-mode--create-heartbeat nil)))
+(defun wakame--handle-save()
+  (wakame--add-heartbeat (wakame--create-heartbeat t)))
 
-(defun wakame-mode--buffer-change(frame)
-  (add-to-list 'wakame-mode--heartbeats (wakame-mode--create-heartbeat nil)))
+(defun wakame--buffer-change(frame)
+  "Handler for buffer focus changes."
+  (wakame--add-heartbeat (wakame--create-heartbeat nil)))
 
-(add-to-list 'window-selection-change-functions 'wakame-mode--buffer-change)
-(add-hook 'after-save-hook
-          (lambda()
-            (add-to-list 'wakame-mode--heartbeats (wakame-mode--create-heartbeat t)))
-          nil t)
+(defun wakame-mode--enable()
+  "Add hooks to enable wakame tracking."
+  (add-to-list 'window-selection-change-functions #'wakame--buffer-change)
+  (add-hook 'first-change-hook 'wakame--buffer-change nil t)
+  (add-hook 'after-save-hook #'wakame-handle-save nil t))
+
+(defun wakame-mode--disable()
+  "Remove hooks and disable wakame tracking."
+  (setq window-selection-change-functions (delete #'wakame--buffer-change window-selection-change-functions))
+  (remove-hook 'after-save-hook #'wakame--handle-save t)
+  (remove-hook 'first-change-hook #'wakame--buffer-change t))
+
+;;;###autoload
+(define-minor-mode wakame-mode
+  "Toggle WakaMeMode (WakaTime Native mode)."
+  :lighter    " waka"
+  :init-value nil
+  :global     nil
+  :group      'wakame
+  (cond
+    (noninteractive (setq wakame-mode nil))
+    (wakame-mode (wakame-mode--enable))
+    (t (wakame-mode--disable))))
+
+;;;###autoload
+(define-globalized-minor-mode global-wakame-mode wakame-mode (lambda () (wakame-mode 1)))
 
 (provide 'wakame-mode)
 ;;; wakame-mode.el ends here
